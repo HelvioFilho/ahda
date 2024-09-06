f3899e9a2fb9c8899344ade933357f1f07a1c10f
Hélvio Filho <helviosvf@gmail.com>
2024-09-06 18:05:34 -0300

    style: update Header component image height
    - Adjusted image height from h-28 to h-44 in the Header component for better visual appearance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Logo from "@/assets/angel-white.png";
 export function Header() {
   return (
     <View className="bg-light border-b border-header pt-5 pb-4 mb-1">
-      <Image className="w-full h-28" resizeMode="contain" source={Logo} />
+      <Image className="w-full h-44" resizeMode="contain" source={Logo} />
       <Text className="text-center text-base font-bold">
         Programa A hora do Anjo{"\n"}De segunda à sexta de 18h às 19h
       </Text>
