17af5815ae85bc2a14e81fd9dbb08ee00241535b
Hélvio Filho <helviosvf@gmail.com>
2024-09-06 18:09:55 -0300

    feat: create Home screen with initial post data and layout
    - Added the Home screen component with a list of posts using FlatList
    - Integrated Header and PostList components
    - Included initial mock post data

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,51 @@
-import { Text, View } from "react-native";
+import { Header } from "@/components/Header";
+import { PostList } from "@/components/PostList";
+import { FlatList, Text, View } from "react-native";
+
+export type PostProps = {
+  id: string;
+  title: string;
+  text: string;
+  preview: string;
+  user: {
+    name: string;
+    image: string;
+    about: string;
+  };
+  youtube: string;
+  cover: string;
+  date_post: string;
+};
 
 export function Home() {
+  const posts: PostProps[] = [
+    {
+      id: "1",
+      title: "Curiosidades do Cristianismo",
+      text: "Text 1",
+      preview:
+        "A primeira igreja católica de Belém, conhecida como a Catedral Metropolitana de Belém, ou Catedral da Sé, foi fundada em 1616, logo após a fundação da cidade pelos portugueses.",
+      user: {
+        name: "Hélvio Filho",
+        image: "https://avatars.githubusercontent.com/u/14864367",
+        about: "About 1",
+      },
+      youtube: "https://www.youtube.com/watch?v=Phj08UZbCUs",
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/d/da/Belem-Merces2.jpg",
+      date_post: "17/02/2023 10:25",
+    },
+  ];
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold">Home</Text>
+    <View className="flex-1 w-full bg-background">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PostList data={item} />}
+        ListHeaderComponent={() => <Header />}
+        ListEmptyComponent={() => <Text>Empty</Text>}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
