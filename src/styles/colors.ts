ab90d95936f2ee11d0836b5671d3a0ee33363efd
Hélvio Filho <helviosvf@gmail.com>
2024-09-06 14:40:19 -0300

    chore: set up NativeWind with custom configurations
    - Installed and configured NativeWind
    - Added @types with nativewind-env.d.ts file
    - Integrated Roboto font with NativeWind
    - Configured custom color schemes for the project

---
+++ src/styles/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  textDefault: "#0F0F0F",
+  textMid: "#3E3E3E",
+  textLight: "#CFCFCF",
+
+  inputField: "#3D434D",
+  inputBorder: "#E3E4E5",
+
+  light: "#FFFFFF",
+  dark: "#000000",
+
+  background: "#F1F1F1",
+  backgroundModal: "rgba(0,0,0,0.8)",
+  mark: "#F64F64",
+  splash: "#CAEAF5",
+
+  tabBarColor: {
+    active: "#4B92D4",
+    player: "#FFFFFF",
+    shadow: "#7F5DF0",
+  },
+
+  success: "#4DCFC0",
+  warning: "#EEA63A",
+  error: "#E83F5B",
+};
