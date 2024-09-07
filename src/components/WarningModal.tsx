eb154ba0b9c1d90cf30beeba8c2fb9c51c13471f
Hélvio Filho <helviosvf@gmail.com>
2024-09-07 11:04:36 -0300

    feat: create WarningModal component for alerts
    - Implemented WarningModal component to display warning messages
    - Supports customizable title, message, height, and button color
    - Integrated close functionality with a pressable close button

---
+++ src/components/WarningModal.tsx
@@ -0,0 +1,54 @@
+import { Pressable, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { colors } from "@/styles/colors";
+
+type WarningModalProps = {
+  title?: string;
+  message: string;
+  height?: number;
+  colorButton: string;
+  closeModal: () => void;
+};
+
+export function WarningModal({
+  title = "Aviso",
+  message,
+  height = 200,
+  colorButton,
+  closeModal,
+}: WarningModalProps) {
+  return (
+    <View className="flex-1 h-full justify-center items-center bg-backgroundModal">
+      <View
+        className={`
+          w-11/12 
+          bg-light 
+          rounded-3xl 
+          p-4 
+          justify-around 
+          items-center
+        `}
+        style={{ height: height }}
+      >
+        <Pressable
+          onPress={closeModal}
+          className="absolute top-2 right-2 p-2 z-20"
+        >
+          <Ionicons name="close-circle" size={30} color={colors.error} />
+        </Pressable>
+        {title && <Text className="text-xl font-bold text-dark">{title}</Text>}
+        <Text className="w-4/5 text-base font-regular">{message}</Text>
+        <View className="w-11/12 h-12 py-5 px-8 justify-around items-center">
+          <Pressable
+            className="w-11/12 h-10 justify-center items-center rounded-xl bg"
+            style={{ backgroundColor: colorButton }}
+            onPress={closeModal}
+          >
+            <Text className="text-base font-regular text-light">Fechar</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+}
