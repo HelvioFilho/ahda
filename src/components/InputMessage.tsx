f8da06c711afff20ff52f085d26330424a970b3b
Hélvio Filho <helviosvf@gmail.com>
2024-09-06 22:01:20 -0300

    feat: create InputMessage component for form handling
    - Added InputMessage component using react-hook-form's Controller
    - Supports label, error display, and dynamic height adjustment
    - Designed for flexible use with optional children for additional UI elements

---
+++ src/components/InputMessage.tsx
@@ -0,0 +1,60 @@
+import { Text, TextInput, TextInputProps, View } from "react-native";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+type InputMessageProps<T extends FieldValues> = TextInputProps & {
+  name: Path<T>;
+  label: string;
+  control: Control<T>;
+  error: string | undefined;
+  changeHeight?: number;
+  children?: React.ReactNode;
+};
+
+export function InputMessage<T extends FieldValues>({
+  name,
+  label,
+  control,
+  error,
+  changeHeight = 50,
+  children,
+  ...rest
+}: InputMessageProps<T>) {
+  return (
+    <View className="mb-4">
+      <Text className="text-base text-textDefault font-medium mb-2">
+        {label}
+      </Text>
+      <View
+        className={`
+            w-full
+            flex-row
+            items-center
+            px-5
+            py-2.5
+            rounded-md
+            bg-light
+            border
+            border-inputBorder
+            h-[${changeHeight}px]
+          `}
+      >
+        <Controller
+          control={control}
+          name={name}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              className="flex-1 h-full text-base text-inputField font-regular"
+              onChangeText={onChange}
+              value={value}
+              {...rest}
+            />
+          )}
+        />
+        {children && children}
+      </View>
+      {error && (
+        <Text className="text-base text-error font-regular mt-1">{error}</Text>
+      )}
+    </View>
+  );
+}
