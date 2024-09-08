c03a07bd43c74773da3af85efe27a747fd052407
Hélvio Filho <helviosvf@gmail.com>
2024-09-08 14:16:54 -0300

    chore: create playback service for TrackPlayer
    - Added 'playbackService.ts' in src/utils/service to handle TrackPlayer events
    - Implemented listeners for 'RemotePlay' and 'RemotePause' events
    - Configured TrackPlayer to play, pause, and reset based on remote control inputs

---
+++ src/utils/playbackService.ts
@@ -0,0 +1,11 @@
+import TrackPlayer, { Event } from "react-native-track-player";
+
+export async function playbackService() {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+    TrackPlayer.reset();
+  });
+}
