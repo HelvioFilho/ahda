b24c6c6c3a778e46b8d99c340ad3429432451489
Hélvio Filho <helviosvf@gmail.com>
2024-09-10 10:45:24 -0300

    feat: add navigation to PostList component
    - Integrated navigation to PostList allowing users to navigate to the Post screen
    - Implemented setPost function to store the selected post data in the global store
    - Added loading state to display a loading indicator until the post cover image is fully loaded

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,20 +1,50 @@
 import { PostProps } from "@/screens/Home";
+import { appDataStore } from "@/services/store";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { Image, Pressable, Text, View } from "react-native";
+import { Loading } from "./Loading";
 
 type PostListProps = {
   data: PostProps;
 };
 
 export function PostList({ data }: PostListProps) {
+  const [loading, setLoading] = useState(true);
   const date = data.date_post.split(" ");
+  const { navigate } = useNavigation();
+  const { setPost } = appDataStore();
+
+  function handleSetPost() {
+    setPost(data);
+    navigate("Post");
+  }
 
   return (
-    <Pressable className="w-full py-2.5 rounded-ee-3xl bg-light mb-5">
+    <Pressable
+      className="w-full py-2.5 rounded-ee-3xl bg-light mb-5"
+      onPress={handleSetPost}
+    >
       <Text className="text-2xl font-bold text-center text-dark mb-2">
         {data.title}
       </Text>
       <View className="relative items-center w-full h-64">
-        <Image className="w-full h-full" source={{ uri: data.cover }} />
+        {loading && (
+          <Loading
+            style={{
+              position: "absolute",
+              top: 250 / 2.5,
+              elevation: 999,
+              zIndex: 999,
+            }}
+            size={32}
+          />
+        )}
+        <Image
+          className="w-full h-full"
+          source={{ uri: data.cover }}
+          onLoadEnd={() => setLoading(false)}
+        />
       </View>
       {data.user.name ? (
         <View className="-top-5 items-center justify-center w-1/2 rounded-r-2xl bg-mark">
